b01b13583577786226da350f84348ab1367d487c
Brosa <brunoarosa1997@gmail.com>
2023-02-15 12:00:31 -0500

    added question variables, created basic html for index and high score file

---
+++ assets/questions.js
@@ -0,0 +1,29 @@
+var question1 = {
+    question: "Commonly used data types DO Not Include:",
+    options: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+    answers: "3"
+}
+
+var question2 = {
+    question: "The condition in an if/else statement is enclosed with __________.",
+    options: ["1. quotes", "2. curly brackets", "3. parenthesis", "4. square brackets"],
+    answers: "3"
+}
+
+var question3 = {
+    question: "Arrays in JavaScript can be used to store",
+    options: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
+    answers: "4"
+}
+
+var question4 = {
+    question: "String values must be enclosed within __________ when being assigned to variables.",
+    options: ["1. commas", "2. curly brackets", "3. quotes", "4. parenthesis"],
+    answers: "3"
+}
+
+var question5 = {
+    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    options: ["1. JavaScript", "2. terminal/bash", "3. for loops", "4.console.log"],
+    answers: "4"
+}
