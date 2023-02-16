1b9a89b952a173ce33857c85c49421b62c33e75d
Brosa <brunoarosa1997@gmail.com>
2023-02-16 03:24:44 -0500

    added some elements to .js and set attributes to them

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,19 @@
+//
+var mainEl = document.querySelector("main")
+var h1El = document.querySelector("h1")
+var pEl = document.querySelector("p")
+var startBtnEl = document.querySelector(".start-btn")
+
+mainEl.setAttribute("style", "text-align: center")
+h1El.setAttribute("style", "font-size: 4em")
+pEl.setAttribute("style", "font-size: 2em; margin: 5% 20%")
+startBtnEl.setAttribute("style", "font-size: 2em; background-color: green")
 //testing link of questions js
 console.log(question1)
+console.log(question1.question)
 console.log(question1["question"])
-console.log(question1["options"])
-console.log(question1["answers"])
+console.log(question1.options)
+console.log(question1.answers)
+console.log(question1.options[0]) //how to access array in object
+
+//think about doing the css in js for practice
