d923a27bac2bdf9c6cb07f350e73188952785a16
Brosa <brunoarosa1997@gmail.com>
2023-02-17 06:14:53 -0500

    created a function that adds a question and its options to html

--- assets/script.js
+++ assets/script.js
@@ -3,11 +3,15 @@ var mainEl = document.querySelector("main")
 var h1El = document.querySelector("h1")
 var pEl = document.querySelector("p")
 var startBtnEl = document.querySelector(".start-btn")
+var questionBoxEl = document.querySelector(".question-box")
 
 mainEl.setAttribute("style", "text-align: center")
 h1El.setAttribute("style", "font-size: 4em")
 pEl.setAttribute("style", "font-size: 2em; margin: 5% 20%")
 startBtnEl.setAttribute("style", "font-size: 2em; background-color: green")
+
+
+
 //testing link of questions js
 console.log(question1)
 console.log(question1.question)
@@ -16,4 +20,31 @@ console.log(question1.options)
 console.log(question1.answers)
 console.log(question1.options[0]) //how to access array in object
 
-//think about doing the css in js for practice
+
+function styleQuestion(obj) {
+    // Styling Question
+    var questionStyle = document.createElement("h2");
+    questionStyle.textContent = Object.values(obj)[0];
+    questionBoxEl.appendChild(questionStyle);
+
+    // Styling Options
+    var optionStyleparent = document.createElement("ul")
+    questionBoxEl.appendChild(optionStyleparent)
+
+    function grabOptions(arr) {
+        for (let i = 0; i < arr.length; i++) {
+            var optionStyle = document.createElement("li")
+            optionStyle.textContent = arr[i]
+            optionStyleparent.appendChild(optionStyle)
+        }
+    }
+
+    grabOptions(Object.values(obj)[1])
+}
+
+
+/* function startQuiz(){
+    styleQuestion()
+
+} */
+styleQuestion(question1)
