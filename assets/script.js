ecb04aa8eb6a12a4dd0bd9af1255588aff4950dd
Brosa <brunoarosa1997@gmail.com>
2023-02-18 13:35:01 -0500

    repurposed styleQuestion and improved optionsEl event listener

--- assets/script.js
+++ assets/script.js
@@ -7,6 +7,8 @@ var h1El = document.querySelector("h1")
 var pEl = document.querySelector("p")
 var startBtnEl = document.querySelector(".start-btn")
 var questionBoxEl = document.querySelector(".question-box")
+var questionPromptEl = document.querySelector(".question-prompt")
+var optionsEl = document.getElementById("options")
 
 //Styling of DOM elements
 mainEl.setAttribute("style", "text-align: center")
@@ -14,9 +16,11 @@ headerEl.setAttribute("style", "display: flex; justify-content: space-between; p
 h1El.setAttribute("style", "font-size: 4em")
 pEl.setAttribute("style", "font-size: 2em; margin: 5% 20%")
 startBtnEl.setAttribute("style", "font-size: 2em; background-color: green")
+optionsEl.setAttribute("style", "")
 
 var secondsLeft = 75
 var ptsScored = 0
+var Qindex = 0
 
 //testing link of questions js
 console.log(arrayOfQuestions[0])
@@ -31,7 +35,7 @@ function timer() {
             secondsLeft--
             timerEl.textContent = "Time:" + secondsLeft;
 
-            if (secondsLeft === 0) { 
+            if (secondsLeft === 0) {
                 // when it hits zero I need to go to high score
                 // Need to clear interval if all questions are answered
                 clearInterval(timerInterval)
@@ -43,51 +47,46 @@ function clearPrompt() {
     IntroEl.remove()
 }
 // TODO: rename function
-function styleQuestion(obj) {
+function printQuestion(obj) {
     // Styling Question
-    var questionStyle = document.createElement("h2");
-    questionStyle.textContent = Object.values(obj)[0];
-    questionBoxEl.appendChild(questionStyle);
-    questionStyle.setAttribute("style", "font-size: 3em; ")
-
-    // creates the parent element to append the options to
-    var optionStyleparent = document.createElement("ul")
-    questionBoxEl.appendChild(optionStyleparent)
-    optionStyleparent.classList.add("ListOfOptions") // ?? might not need this class
+    questionPromptEl.textContent = Object.values(obj)[0];
+    questionPromptEl.setAttribute("style", "font-size: 3em; ")
 
     // creates and styles the list of options
     var arrayOfOptions = Object.values(obj)[1]; // I feel like I need to be more specific
-    var answer = Object.values(obj)[2]
-    var selectedResponse = document.createElement("p")
-    questionBoxEl.appendChild(selectedResponse)
 
     for (let i = 0; i < arrayOfOptions.length; i++) {
         var optionStyle = document.createElement("li")
         optionStyle.textContent = arrayOfOptions[i]
-        optionStyleparent.appendChild(optionStyle)
-        optionStyle.classList.add("options") // ?? might not need this class
-        optionStyle.setAttribute("style", " display: inline; ; background-color: navy; padding: .2em .6em; margin: .6em; border: 2px solid navy; border-radius: 10px; font-size: 2em; color: white;")
+        optionsEl.appendChild(optionStyle)
+        optionStyle.classList.add("optionsTest") // ?? might not need this class
+        optionStyle.setAttribute("style", " background-color: navy; padding: .2em .6em; margin: .6em; border: 2px solid navy; border-radius: 10px; font-size: 1.5em; color: white;")
     }
 
-    var optionsEl = document.querySelector("ul") //Don't really like! Should use .options
-    // Adds an event listener to each option
-    optionsEl.addEventListener("click", function (event) {
-        if (event.target.textContent === answer) {
-            selectedResponse.textContent = "Right!"
-            ptsScored = ptsScored + 10
-            console.log(ptsScored)
-        }
-        else {
-            selectedResponse.textContent = "Wrong!"
-            secondsLeft = secondsLeft - 10
-        }
-    })
-
 }
+// Adds an event listener to each option
+optionsEl.addEventListener("click", function (event) {
+    
+    var selectedResponse = document.createElement("p")
+    questionBoxEl.appendChild(selectedResponse)
+
+    if (event.target.textContent === arrayOfQuestions[Qindex].answers) {
+        selectedResponse.textContent = "Right!"
+        ptsScored = ptsScored + 10
+        console.log(ptsScored)
+    }
+    else {
+        selectedResponse.textContent = "Wrong!"
+        secondsLeft = secondsLeft - 10
+    }
+    optionsEl.remove()
+    Qindex++
+    printQuestion(arrayOfQuestions[Qindex])
+})
 
 function startQuiz() {
     clearPrompt();
-    styleQuestion(arrayOfQuestions[0]);
+    printQuestion(arrayOfQuestions[Qindex]);
     timer();
 }
 
